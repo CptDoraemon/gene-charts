9f66fdbde766967c62170056e49cfa2982b4004e
Xiaoxi <yxx0827@gmail.com>
2020-08-13 13:09:04 -0400

    improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        path.resolve(__dirname, 'src/index.js'),
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].[contenthash].bundle.js',
+        chunkFilename: '[name].[contenthash].chunk.js',
         path: path.resolve(__dirname, 'examples'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Gene Charts',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        moduleIds: 'hashed',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+        runtimeChunk: 'single',
     },
     devServer: {
         contentBase: path.join(__dirname, 'examples'),
@@ -12,5 +36,4 @@ module.exports = {
         port: 3000
     },
     mode: 'production',
-    devtool: 'source-map'
 };
